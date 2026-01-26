d3167744dc0c306227d7137b7902aa2adb2ecb38
Nilesh Pataskar <nileshpataskars@gmail.com>
2026-01-26 21:53:22 +0530

    Revert "about us svg"
    This reverts commit 86d21b186a7fc7ecf52af3cb179b5a080d282d32.

--- app/src/component/about/HeroSection.tsx
+++ app/src/component/about/HeroSection.tsx
@@ -2,35 +2,19 @@
 
 import { motion } from 'motion/react'
 
-type Props = {
-  svgContent?: string | null;
-}
-
-const HeroSection = ({ svgContent }: Props) => {
+const HeroSection = () => {
   return (
     <section className='relative bg-white h-full '>
       <div className="min-h-[80vh] flex items-center relative overflow-hidden">
-        {svgContent ? (
-          <div 
-            className="absolute inset-0 w-full h-full"
-            dangerouslySetInnerHTML={{ 
-              __html: svgContent.replace(
-                '<svg',
-                '<svg style="width: 100%; height: 100%; object-fit: cover; position: absolute; top: 0; left: 0;"'
-              )
-            }} 
-          />
-        ) : (
-          <div 
-            className="absolute inset-0 w-full h-full"
-            style={{
-              backgroundImage: 'url(/about/hero.svg)',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat'
-            }}
-          />
-        )}
+        <div 
+          className="absolute inset-0 w-full h-full"
+          style={{
+            backgroundImage: 'url(/about/hero.svg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat'
+          }}
+        />
       </div>
     </section>
   )
